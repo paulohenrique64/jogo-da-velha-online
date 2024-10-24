66f76f7276f736e6ef6033dba1bbec9f1dc7f697
paulohenrique64 <paulohenriquelvs20@gmail.com>
2024-10-24 18:30:54 -0300

    changed home page Menu redirection

--- public/javascript/home.js
+++ public/javascript/home.js
@@ -1,11 +1,3 @@
-// rolar até a seção "Home" quando clicar no link "Home" no cabeçalho
-document.getElementById("menuLink").addEventListener("click", function(event) {
-  event.preventDefault();
-  
-  const menuSection = document.getElementById("menu");
-  menuSection.scrollIntoView({ behavior: "smooth" });
-});
-
 // rolar até a seção "About" quando clicar no link "About" no cabeçalho
 document.getElementById("aboutLink").addEventListener("click", function(event) {
     event.preventDefault(); 
